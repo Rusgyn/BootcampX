3b8efb1a2e39907a2d009294142174087a0fdb02
rusgyn <rusgynmagsanay@gmail.com>
2024-03-22 01:05:51 -0400

    Cohorts with 18 or more students.

---
+++ 3_queries_group-by/3_large_cohorts.sql
@@ -0,0 +1,8 @@
+-- Get all cohorts with 18 or more students.
+
+SELECT cohorts.name as cohort_name, count(students.*) as student_count
+FROM students
+JOIN cohorts ON cohorts.id = cohort_id
+GROUP BY cohorts.name
+HAVING count(students.*) >= 18
+ORDER BY count(students.*) ASC;
