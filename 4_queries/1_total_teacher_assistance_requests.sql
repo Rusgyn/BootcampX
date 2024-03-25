92e47c98cc40413c2f62c813576dc0c619bbd853
rusgyn <rusgynmagsanay@gmail.com>
2024-03-25 02:15:40 -0400

    Query that get the total number of assistance_requests for a student.

--- 4_queries/1_total_teacher_assistance_requests.sql
+++ 4_queries/1_total_teacher_assistance_requests.sql
@@ -1,5 +1,4 @@
 -- Get the total number of assistance_requests for a teacher.
-
 SELECT count(assistance_requests.*) as total_assistances, teachers.name
 FROM assistance_requests
 JOIN teachers ON teachers.id = teacher_id
