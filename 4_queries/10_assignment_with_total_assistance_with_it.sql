34abd95e23723ad55d7702fd471caa63e9a2babc
rusgyn <rusgynmagsanay@gmail.com>
2024-03-25 08:22:46 -0400

    Implement query that shows the assignmets total number of assistance requests.

---
+++ 4_queries/10_assignment_with_total_assistance_with_it.sql
@@ -0,0 +1,7 @@
+-- List each assignment with the total number of assistance requests with it.
+
+SELECT assignments.id as id, assignments.name as name, assignments.day as day, assignments.chapter as chapter, count(assistance_requests.id) as total_requests
+FROM assistance_requests
+JOIN assignments ON assignments.id = assignment_id
+GROUP BY assignments.id
+ORDER BY total_requests DESC;
