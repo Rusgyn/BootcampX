c6660ace6bd78d221207d24401351e24281449ea
rusgyn <rusgynmagsanay@gmail.com>
2024-03-24 22:16:18 -0400

    Low average completion time

--- 3_queries_group-by/5_average_completion_time.sql
+++ 3_queries_group-by/5_average_completion_time.sql
@@ -5,4 +5,4 @@ FROM students
 JOIN assignment_submissions ON student_id = students.id
 WHERE end_date IS NULL
 GROUP BY students.name
-ORDER BY avg(assignment_submissions.duration)DESC;
+ORDER BY avg(assignment_submissions.duration) DESC;
