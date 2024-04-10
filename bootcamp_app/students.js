58e5ea766b6db33a8302e5c231133cd74a4bfe3f
rusgyn <rusgynmagsanay@gmail.com>
2024-04-10 13:40:02 -0400

    Use parameterized query to avoid SQL injection vulnerabilities.

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -15,43 +15,27 @@ pool.connect(function(err) {
   console.log("Connected!");
 });
 
-/**
- * pool.query is a function that accepts an SQL query as a JavaScript string
- * Using the ` (backtick), we can write a multi line string like this to make our SQL look nicer. 
- * The function then returns a promise that contains our result when the query is successful.
- */
-pool
-  .query(
-    
-    // SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-    // FROM students
-    // JOIN cohorts
-    // ON students.cohort_id = cohorts.id
-    // LIMIT 2;
-    //OR BELOW CODE WITH PROCESS.ARGV, RUN AS node students.js 3 + Enter Key. The 3 is the limit (LIMIT 3;)
-    `
-    SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-    FROM students
-    JOIN cohorts ON cohorts.id = cohort_id
-    WHERE cohorts.name LIKE '%${process.argv[2]}%'
-    LIMIT ${process.argv[3] || 5};
-    `
-  )
-  
-  // .then((res) => {
-  //   console.log(res.rows);
-  // })
-  // .catch((err) => console.error("query error", err.stack));
-  /**EXPECTED RESULT:
-   * [
-  { id: 1, name: 'Armand Hilll', cohort_id: 1 },
-  { id: 2, name: 'Stephanie Wolff', cohort_id: 1 },
-  { id: 3, name: 'Stan Miller', cohort_id: 1 },
-  { id: 4, name: 'Elliot Dickinson', cohort_id: 1 },
-  { id: 5, name: 'Lloyd Boehm', cohort_id: 1 }
-]
-  OR BELOW CODE */
+/**Using parameterized query
+ * 
+ * The $1 and $2 placeholders will eventually get replaced with the actual
+ * data from the values array. 
+*/
 
+const queryString = `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `;
+
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+//Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`, limit];
+
+pool
+  .query(queryString, values)
   .then((res) => {
     res.rows.forEach((user) => {
       console.log(
@@ -61,14 +45,37 @@ pool
   })
   .catch ((err) => console.error("query error", err.stack));
 
-  /** EXPECTED RESULT
-   * Armand Hilll has an id of 1 and was in the 1 cohort
-students.js:37
-Stephanie Wolff has an id of 2 and was in the 1 cohort
-students.js:37
-Stan Miller has an id of 3 and was in the 1 cohort
-students.js:37
-Elliot Dickinson has an id of 4 and was in the 1 cohort
-students.js:37
-Lloyd Boehm has an id of 5 and was in the 1 cohort
-   */
+//INPUT: Run as 
+// node students.js FEB 3 + Enter Key
+//OUTPUT: 
+// Stephanie Wolff has an id of 2 and was in the FEB12 cohort
+// Stan Miller has an id of 3 and was in the FEB12 cohort
+// Elliot Dickinson has an id of 4 and was in the FEB12 cohort
+
+
+//====================================================
+
+/**Using node-postgres 
+ * 
+ * pool.query is a function that accepts an SQL query as a JavaScript string
+*/
+
+// pool
+//   .query(
+//     `
+//     SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+//     FROM students
+//     JOIN cohorts ON cohorts.id = cohort_id
+//     WHERE cohorts.name LIKE '%${process.argv[2]}%'
+//     LIMIT ${process.argv[3] || 5};
+//     `
+//     //RUN AS node students.js 4 + Enter Key. The 4 is the limit (LIMIT 4;)
+// //   )
+//   .then((res) => {
+//     res.rows.forEach((user) => {
+//       console.log(
+//         `${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`
+//       );
+//     });
+//   })
+//   .catch ((err) => console.error("query error", err.stack));
