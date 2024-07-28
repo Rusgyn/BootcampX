79780eba4d9c5a825643bcc052162c407bff3ffd
rusgyn <rusgynmagsanay@gmail.com>
2024-07-28 17:52:53 -0400

    SQL that generates the total number of students in first 3 cohorts

---
+++ 0_selects/3_total_students_in_cohorts.sql
@@ -0,0 +1,7 @@
+-- The admissions department wants to know the total number of students from any number of cohorts combined.
+
+-- Select the total number of students who were in the first 3 cohorts.
+
+SELECT count(*)
+  FROM students
+  WHERE cohort_id IN (1, 2, 3);
