f354de57e27862ddfc6639661d3024e1e6ee1896
rusgyn <rusgynmagsanay@gmail.com>
2024-08-06 15:23:34 -0400

    Modify query parameters and use process.argv in declaring values.

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -9,14 +9,18 @@ const pool = new Pool({
   database: "bootcampx",
 });
 
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+
 pool
   .query(
     `
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students
-JOIN cohorts ON cohorts.id = cohort_id
-LIMIT 5;
-`
+      SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+      FROM students
+      JOIN cohorts ON cohorts.id = cohort_id
+      WHERE cohorts.name LIKE $1
+      LIMIT $2;
+      `, [`%${cohortName}%`, `${limit}`]
   )
   .then((res) => {
     res.rows.forEach((user) => {
